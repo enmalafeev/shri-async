28227acc75bd1a7c0c7581c0f4be744167de4f5a
enmalafeev <malafeev.en@yandex.ru>
2021-10-15 20:26:18 +0300

    refactoring

--- solution/index.js
+++ solution/index.js
@@ -1,31 +1,32 @@
 module.exports = function (Homework) {
   const { add, less } = Homework;
 
+  const promisify = (asyncFn) => (...args) => {
+    const promise = new Promise((resolve) => {
+        asyncFn(...args, (data) => resolve(data));
+    });
+    return promise;
+  };
+
+  const promiseLess = promisify(less);
+  const promiseAdd = promisify(add);
+
   return async (asyncArray, fn, initialValue, cb) => {
-
-    const promisify = (asyncFn) => (...args) => {
-      const promise = new Promise((resolve) => {
-          asyncFn(...args, (data) => resolve(data));
-      });
-      return promise;
-    };
-
     const { get, length } = asyncArray;
-
     const promiseLength = promisify(length);
-    const promiseLess = promisify(less);
     const promiseGet = promisify(get);
-    const promiseAdd = promisify(add);
-
     const promiseFn = promisify(fn);
 
-    const len = await promiseLength();
-
-    for (let i = 0; await promiseLess(i, len); i = await promiseAdd(i, 1)) {
-      let currentItem = await promiseGet(i);
-      initialValue = await promiseFn(initialValue, currentItem, i, asyncArray, cb);
+    const inner = async () => {
+      const len = await promiseLength();
+      for (let i = 0; await promiseLess(i, len); i = await promiseAdd(i, 1)) {
+        let currentItem = await promiseGet(i);
+        initialValue = await promiseFn(initialValue, currentItem, i, asyncArray);
+      }
+      return initialValue;
     }
 
-    cb(initialValue);
+    const res = await inner();
+    cb(res);
   }
 }
